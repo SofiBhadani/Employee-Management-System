e6d28401fb611f80226b2a7fdabaf66279ef51ff
Sofi Pradeep Bhadani <54234451+SofiBhadani@users.noreply.github.com>
2019-09-24 03:58:27 -0700

    AngularJS

---
+++ src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { EmployeeService } from './employee.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  constructor(private emp: EmployeeService){}
+  title = 'employee-sofi';
+  emp_id
+  emp_name
+  emp_dept
+  myemployee={id:1,name:'',dept:''}
+  addEmployee()
+  {
+      this.myemployee.id=this.emp_id
+      this.myemployee.name=this.emp_name
+      this.myemployee.dept=this.emp_dept
+      this.emp.setEmployee(this.myemployee)
+  }
+
+}
